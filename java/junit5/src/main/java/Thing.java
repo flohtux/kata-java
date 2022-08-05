088a6902c68177ccd858f541bb3ca3912ac27d78
FreddyT <freddy_temme@web.de>
2022-08-05 11:34:34 +0000

    Implementation of calculator Kata

--- java/junit5/src/main/java/Thing.java
+++ java/junit5/src/main/java/Thing.java
@@ -1,6 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Thing {
 
-    public String callForAction() {
-        return "Dog";
+    public int add(String numbers){
+
+        int result = 0;
+        String delimiter = "[,\n]";
+        String[] zahlen;
+        List<String> negatives = new ArrayList<>();
+
+        if(numbers.equals("")){
+            return result;
+        }
+        if(numbers.startsWith("//")){
+            String[] konfiguartionen = numbers.split("[//\n]");
+            delimiter = konfiguartionen[2];
+            numbers = konfiguartionen[3];   
+        }
+
+        zahlen = numbers.split(delimiter);
+
+        for(String zahl: zahlen){
+            int number = Integer.parseInt(zahl);
+            
+            if(number < 0){
+                negatives.add(zahl);
+            } else{
+                result += number;
+            }
+
+        }
+
+       // if(notEmpty(negatives))
+
+        return result;
     }
 }
--- java/junit5/src/test/java/ThingTest.java
+++ java/junit5/src/test/java/ThingTest.java
@@ -1,21 +1,55 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
+
 class ThingTest {
 
+    Thing thing = new Thing();
+
     @Test
-    void fail() {
-        Thing thing = new Thing();
-        String value = thing.callForAction();
-        assertThat(value)
-                .isEqualTo("Food");
+    void emptyString(){
+        assertThat(thing.add("")).isEqualTo(0);
     }
 
     @Test
-    void it_should_not_fail() {
-        assertThat(42)
-                .isEqualTo(42);
+    void addOneNumber(){
+        assertThat(thing.add("1")).isEqualTo(1);
+    }
+
+    @Test
+    void addOneAndTwo(){
+        assertThat(thing.add("1,2")).isEqualTo(3);
+    }
+
+    @Test
+    void addManyNumbers(){
+        assertThat(thing.add("1,2,5,4,7")).isEqualTo(19);
+    }
+    
+    @Test
+    void addWithNewLine(){
+        assertThat(thing.add("1\n2,3")).isEqualTo(6);
+    }
+
+    @Test
+    void addWithSemicolonAsDelimiter(){
+        assertThat(thing.add("//;\n1;2")).isEqualTo(3);
+    }
+
+    @Test
+    void addWithOneNegative(){
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> thing.add("-1,5"));
+        assertEquals(exception.getMessage(), "negatives not allowed: -1");
+    }
+    
+    @Test
+    void addWithTwoNegatives(){
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> thing.add("-1,5,-4"));
+        assertEquals(exception.getMessage(), "negatives not allowed: -1, -4");
     }
 }
